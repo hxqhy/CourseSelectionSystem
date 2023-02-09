d2b99e19a18dae31a0933d22e62d58d2711db271
hy <2381307382@qq.com>
2023-02-09 10:34:15 +0800

    first commit

---
+++ src/test/java/com/wt/courseselectionsystem/CourseSelectionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wt.courseselectionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSelectionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
